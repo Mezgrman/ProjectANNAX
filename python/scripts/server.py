a7c37dde6567547c8a0ae7634afc18990bf9a807
Julian Metzler <git@mezgr.de>
2015-08-09 21:51:34 +0200

    Improved font rendering and added configuration saving

--- python/scripts/server.py
+++ python/scripts/server.py
@@ -13,12 +13,14 @@ def main():
         help = "The port for the server to listen on (Default: 1810)")
     parser.add_argument('-d', '--debug', action = 'store_true',
         help = "Enable debug output")
+    parser.add_argument('-cd', '--controller-debug', action = 'store_true',
+        help = "Enable debug output of serial communication")
     parser.add_argument('-ip', '--allowed-ips', type = str,
         help = "A string that each ip that wants to connect has to begin with")
     
     args = parser.parse_args()
-    controller = MatrixController(args.serial_port, baudrate = args.baudrate, debug = args.debug)
-    server = MatrixServer(controller, port = args.port, allowed_ip_match = args.allowed_ips)
+    controller = MatrixController(args.serial_port, baudrate = args.baudrate, debug = args.controller_debug)
+    server = MatrixServer(controller, port = args.port, allowed_ip_match = args.allowed_ips, debug = args.debug)
     server.run()
 
 if __name__ == "__main__":
